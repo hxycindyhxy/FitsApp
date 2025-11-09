9f3b77fe03c155a95a624c9b4a92737137c8922d
hxycindyhxy <xinyi.hu-3@student.uts.edu.au>
2025-11-09 21:19:35 +1100

    - add viewModel for create a variable stepCount to control the count of treeCrown

---
+++ FitsApp/FitsApp/View/TreeCrownView.swift
@@ -0,0 +1,18 @@
+//
+//  TreeCrownView.swift
+//  FitsApp
+//
+//  Created by Xinyi Hu on 9/11/2025.
+//
+
+import SwiftUI
+
+struct TreeCrownView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TreeCrownView()
+}
